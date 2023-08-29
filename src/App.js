4f897298874e3bdabb352a1dac506378d3bdbcb6
Joselynb <joselynmileth29@gmail.com>
2023-08-28 22:23:14 -0500

    Archivos proyecto React

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+import Swipeable from './Components/Swipeable'
+
+import Backg from './Components/Backg';
+import MediaCard from './Components/MediaCard';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Swipeable/>
+      <Backg/>
+      <MediaCard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Backg.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Backg = () => {
+  return (
+    <div className='fondo'>
+      <section className='tittle'><h1 className='ate'>Atenea Adventures</h1>
+      <h2 className='tittle2'>Agencia de viajes</h2></section>
+    </div>
+  )
+}
+
+export default Backg
